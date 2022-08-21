c8915f9480edcd0bb035979106c016e4fc5f975a
denis <9172885049@mail.ru>
2022-08-21 22:27:17 +0300

    update init url

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -6,11 +6,12 @@ import SwiftUI
 public class RemoteImageFetcher: ObservableObject {
     @Published var imageData = Data()
     let url: URL
-    
-    public init(url: URL) {
-        self.url = url
+
+    public init(from url: URL) {
+      self.url = url
     }
-    
+
+
     // 1 Метод fetch, который используется URLSessionдля извлечения данных и установки результата в виде объектов imageData.
     public func fetch() {
         URLSession.shared.dataTask(with: url) { (data, _, _) in
@@ -20,19 +21,19 @@ public class RemoteImageFetcher: ObservableObject {
             }
         }.resume()
     }
-    
+
     // 2 Метод получения данных изображения.
     public func getImageData() -> Data {
         return imageData
     }
-    
+
     // 3 Метод получения URL.
     public func getUrl() -> URL {
         return url
     }
-    
+
     public func purge() {
         imageData = Data()
     }
-    
+
 }
